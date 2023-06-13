2b163796c9936a5af347569fe6eb00b398d49963
Jeffery Morhous <jmorhous@covermymeds.com>
2023-06-13 14:56:52 -0400

    Begin building retirement calculator

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,82 @@
+'use client'
+
 import Image from 'next/image'
+import React, {useEffect, useState} from "react"
 
 export default function Home() {
+  const initialRetirementAge = Number(localStorage.getItem('retirementAge') || 62)
+  const initialRetirementAmount = Number(localStorage.getItem('retirementAmount') || 2000000)
+  const initialAnnualRetirementExpenses = Number(localStorage.getItem('annualRetirementExpenses') || 100000)
+  const initialCurrentAge = Number(localStorage.getItem('currentAge') || 24)
+  const initialCurrentSavings = Number(localStorage.getItem('currentSavings') || 62)
+  const initialContribution = Number(localStorage.getItem('contribution') || 500)
+  const initialContributionFrequency = localStorage.getItem('contributionFrequency') || "Monthly"
+  const initialPreRetROR = Number(localStorage.getItem('preRetROR') || 10)
+  const initialPostRetROR = Number(localStorage.getItem('postRetROR') || 10)
+  const initialInflation = Number(localStorage.getItem('inflation') || 2.9)
+
+  const [retirementAge, setRetirementAge] = useState(initialRetirementAge)
+  const [retirementAmount, setRetirementAmount] = useState(initialRetirementAmount)
+  const [annualRetirementExpenses, setAnnualRetirementExpenses] = useState(initialAnnualRetirementExpenses)
+  const [currentAge, setCurrentAge] = useState(initialCurrentAge)
+  const [currentSavings, setCurrentSavings] = useState(initialCurrentSavings)
+  const [contribution, setContribution] = useState(initialContribution)
+  const [contributionFrequency, setContributionFrequency] = useState(initialContributionFrequency)
+  const [preRetROR, setPreRetROR] = useState(initialPreRetROR)
+  const [postRetROR, setPostRetROR] = useState(initialPostRetROR)
+  const [inflation, setInflation] = useState(initialInflation)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex min-h-screen flex-col items-center p-24 font-mono">
+      <div className="text-3xl max-w-5xl flex  text-sm lg:flex place-items-center">
+        Fire Calculator
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col items-start bg-black text-white">
+        <h2 className="mb-4">You can retire at age: {retirementAge}</h2>
+        <h3 className="mb-4">Target retirement amount: {retirementAmount}</h3>
+        <form className="space-y-4">
+          <label className="flex flex-col">
+            Annual retirement expenses (in today's dollars)
+            <input type="number" className="bg-gray-800 text-white p-2 rounded"></input>
+          </label>
+          <label className="flex flex-col">
+            Current age
+            <input type="number" className="bg-gray-800 text-white p-2 rounded"></input>
+          </label>
+          <label className="flex flex-col">
+            Current savings balance
+            <input type="number" className="bg-gray-800 text-white p-2 rounded"></input>
+          </label>
+          <label className="flex flex-col">
+            Regular contribution
+            <input type="number" className="bg-gray-800 text-white p-2 rounded"></input>
+          </label>
+          <label className="flex flex-col">
+            Contribution frequency
+            <select className="bg-gray-800 text-white p-2 rounded">
+              <option value="Monthly">Monthly</option>
+              <option value="Annually">Annually</option>
+            </select>
+          </label>
+          <div className="space-y-4">
+            <h2 className="mb-4">Advanced Options</h2>
+            <label className="flex flex-col">
+              Pre-retirement rate of return
+              <input type="number" className="bg-gray-800 text-white p-2 rounded"></input>
+            </label>
+            <label className="flex flex-col">
+              Post-retirement rate of return
+              <input type="number" className="bg-gray-800 text-white p-2 rounded"></input>
+            </label>
+            <label className="flex flex-col">
+              Inflation
+              <input type="number" className="bg-gray-800 text-white p-2 rounded"></input>
+            </label>
+          </div>
+        </form>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
